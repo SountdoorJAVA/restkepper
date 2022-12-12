bbf362da4f4d9dec3ceeffce44145b50237a3d7f
morris <1246658372@qq.com>
2022-12-13 02:30:53 +0800

    handler拦截器

--- restkeeper_web/restkeeper_web_common/src/main/java/com/restkeeper/response/interceptor/WebHandlerInterceptor.java
+++ restkeeper_web/restkeeper_web_common/src/main/java/com/restkeeper/response/interceptor/WebHandlerInterceptor.java
@@ -1,6 +1,7 @@
 package com.restkeeper.response.interceptor;
 
 import com.alibaba.nacos.client.utils.StringUtils;
+import com.restkeeper.tenant.TenantContext;
 import com.restkeeper.utils.JWTUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.rpc.RpcContext;
@@ -28,9 +29,16 @@ public class WebHandlerInterceptor implements HandlerInterceptor {
 
         if (StringUtils.isNotEmpty(tokenInfo)) {
             try {
+                //解析jwt令牌
                 Map<String, Object> tokenMap = JWTUtil.decode(tokenInfo);
-                String shopId = (String) tokenMap.get("shopId");
-                RpcContext.getContext().setAttachment("shopId", shopId);
+
+                //String shopId = (String) tokenMap.get("shopId");
+                //将shopId存入RpcContext,RpcContext是dubbo提供的隐式传参的上下文类
+                //RpcContext.getContext().setAttachment("shopId", shopId);
+
+                //因为RpcContext的声明周期是单次调用,所有我们需要自定义一个上下文类TenantContext来存放shopId
+                TenantContext.addAttachments(tokenMap);
+
             } catch (IOException e) {
                 log.error("解析token出错");
             }
