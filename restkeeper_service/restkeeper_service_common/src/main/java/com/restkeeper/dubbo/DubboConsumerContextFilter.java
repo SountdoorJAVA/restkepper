26c120d0e10edbeeb233e8e4470aafd7aeb4e03b
morris <1246658372@qq.com>
2022-12-13 02:34:51 +0800

    dubbo提供的过滤器-调用dubbo是调用

---
+++ restkeeper_service/restkeeper_service_common/src/main/java/com/restkeeper/dubbo/DubboConsumerContextFilter.java
@@ -0,0 +1,29 @@
+package com.restkeeper.dubbo;
+
+import com.restkeeper.tenant.TenantContext;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.common.extension.Activate;
+import org.apache.dubbo.rpc.*;
+
+/**
+ * 每次调用dubbo都会调用该过滤器
+ *
+ * @author MORRIS --> Java
+ * @date 2022-12-13 01:05:33
+ */
+@Activate
+@Slf4j
+public class DubboConsumerContextFilter implements Filter {
+
+    @Override
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+
+        /*log.info("shopId------------" + RpcContext.getContext().getAttachment("shopId"));
+        log.info("ThreadName---------" + Thread.currentThread().getName());*/
+
+        //从自定义的上下文类中获取到令牌的相关信息,然后存入到RpcContext
+        RpcContext.getContext().setAttachment("shopId", TenantContext.getShopId());
+
+        return invoker.invoke(invocation);
+    }
+}
